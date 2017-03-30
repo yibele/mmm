b05c3df0a20e2c3d1b02e0544078479e31f45873
yibeel <yibeel@163.com>
2017-03-30 13:45:47 +0800

    11

--- myzz1/inc/class_PDO.php
+++ myzz1/inc/class_PDO.php
@@ -14,10 +14,37 @@ class Mysql{
         try
         {
             $this->$dsn = $dblib.':host='.$dbhost.';dbname='.$dbname;
-        }catch(PDOException $e)
-        {
+        }catch(PDOException $e){
             exit('mysql connect error'.$e->getMessage());
         }
-        
+    }
+
+
+    public function Query($Sql , $model='many' , $debug = false)
+    {
+        if($debug)
+        {
+            exit($Sql);
+        }
+        $this->job = $this->dbh->query($Sql);
+
+        if($this->job){
+            $this->job->setFetchMode(PDO::FETCH_ASSOC);
+            if($model =='many'){
+                $result = $this->job->fetchAll();
+            }elseif($model =='single'){
+                $result = $this->job->fetch();
+            }
+        }else{
+            return NULL;
+        }
+        $this->job = NULL;
+        return $result;
+    }
+
+    public function Insert($table , $args , $debug = false)
+    {
+        $this->checkFields($table, $args);
     }
 }
+
